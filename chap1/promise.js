6f9b40d2fe52e338317c3af2ba698c07fc14e766
GanYihuan <583520052@qq.com>
2018-09-11 10:35:34 +0800

    first add

---
+++ chap1/promise.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+// function readFileAsync(path) {
+// 	return new Promise((resolve, reject) => {
+// 		fs.readFile(path, (err, data) => {
+// 			if (err) {
+// 				reject(err)
+// 			} else {
+// 				resolve(data)
+// 			}
+// 		})
+// 	})
+// }
+
+// readFileAsync('package.json')
+// 	.then(data => {
+// 		data = JSON.parse(data)
+// 		console.log(data.name)
+// 	})
+// 	.catch(err => {
+// 		console.log(err)
+// 	})
+
+const util = require('util')
+util
+	.promisify(fs.readFile)('./package.json')
+	.then(JSON.parse)
+	.then(data => {
+		console.log(data.name)
+	})
+	.catch(err => {
+		console.log(err)
+	})
---
+++ index.js
@@ -0,0 +1,34 @@
+﻿const fs = require('fs')
+
+// function readFileAsync(path) {
+// 	return new Promise((resolve, reject) => {
+// 		fs.readFile(path, (err, data) => {
+// 			if (err) {
+// 				reject(err)
+// 			} else {
+// 				resolve(data)
+// 			}
+// 		})
+// 	})
+// }
+
+// readFileAsync('package.json')
+// 	.then(data => {
+// 		data = JSON.parse(data)
+// 		console.log(data.name)
+// 	})
+// 	.catch(err => {
+// 		console.log(err)
+// 	})
+
+/* optimize upon code */
+const util = require('util')
+util
+	.promisify(fs.readFile)('./package.json')
+	.then(JSON.parse)
+	.then(data => {
+		console.log(data.name)
+	})
+	.catch(err => {
+		console.log(err)
+	})
